4031b1743ce3752ec95e35c38d77b468a5596ece
Billy Gingrich <whgingrich@gmail.com>
2020-04-30 14:58:30 -0400

    api implemented

---
+++ adventure/api.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+# from pusher import Pusher
+from django.http import JsonResponse
+from decouple import config
+from django.contrib.auth.models import User
+from .models import *
+from rest_framework.decorators import api_view
+from adventure.models import Room
+import json
+
+# instantiate pusher
+# pusher = Pusher(app_id=config('PUSHER_APP_ID'), key=config('PUSHER_KEY'), secret=config('PUSHER_SECRET'), cluster=config('PUSHER_CLUSTER'))
+
+@csrf_exempt
+@api_view(["GET"])
+def initialize(request):
+    user = request.user
+    player = user.player
+    player_id = player.id
+    uuid = player.uuid
+    room = player.room()
+    players = room.playerNames(player_id)
+    return JsonResponse({'uuid': uuid, 'username':player.user.username, 'title':room.name, 'description':room.description, 'item':room.item, 'players':players}, safe=True)
+
+@csrf_exempt
+@api_view(["GET"])
+def rooms(request):
+    
+    rooms = Room.objects.all()
+    room_data = []
+    for room in rooms:
+        room_data.append({"id": room.id, "name": room.name, "description": room.description, "item": room.item, "north": room.n_to, "east": room.e_to, "south": room.s_to, "west": room.w_to, "x": room.x, "y": room.y})
+         
+         
+    return JsonResponse({'num_rooms': len(room_data), 'rooms': room_data}, safe=True)
+
+# @csrf_exempt
+@api_view(["POST"])
+def move(request):
+    dirs={"n": "north", "s": "south", "e": "east", "w": "west"}
+    reverse_dirs = {"n": "south", "s": "north", "e": "west", "w": "east"}
+    player = request.user.player
+    player_id = player.id
+    player_uuid = player.uuid
+    data = json.loads(request.body)
+    direction = data['direction']
+    room = player.room()
+    nextRoomID = None
+    if direction == "n":
+        nextRoomID = room.n_to
+    elif direction == "s":
+        nextRoomID = room.s_to
+    elif direction == "e":
+        nextRoomID = room.e_to
+    elif direction == "w":
+        nextRoomID = room.w_to
+    if nextRoomID is not None and nextRoomID > 0:
+        nextRoom = Room.objects.get(id=nextRoomID)
+        player.currentRoom=nextRoomID
+        player.save()
+        players = nextRoom.playerNames(player_id)
+        currentPlayerUUIDs = room.playerUUIDs(player_id)
+        nextPlayerUUIDs = nextRoom.playerUUIDs(player_id)
+        # for p_uuid in currentPlayerUUIDs:
+        #     pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has walked {dirs[direction]}.'})
+        # for p_uuid in nextPlayerUUIDs:
+        #     pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has entered from the {reverse_dirs[direction]}.'})
+        return JsonResponse({'username':player.user.username, 'name':nextRoom.name, 'description':nextRoom.description, 'item': item, 'players':players, 'error_msg':""}, safe=True)
+    else:
+        players = room.playerNames(player_id)
+        return JsonResponse({'username':player.user.username, 'name':room.name, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
+
+
+@csrf_exempt
+@api_view(["POST"])
+def say(request):
+    # IMPLEMENT
+    return JsonResponse({'error':"Not yet implemented"}, safe=True, status=500)
---
+++ adventure/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import api
+
+
+urlpatterns = [
+    url('init', api.initialize),
+    url('move', api.move),
+    url('rooms', api.rooms),
+    url('say', api.say),
+]
+
+
+   
